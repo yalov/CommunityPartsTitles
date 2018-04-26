b8714f33e473aaa58a69533882d9ab3ebaa22f6c
Alexander Yalov <alexander.yalov@gmail.com>
2018-04-26 23:36:54 +0300

    deploy.bat

---
+++ deploy.bat
@@ -0,0 +1,30 @@
+echo off
+
+set MODNAME=CommunityPartsTitles
+set VERSIONFILE=GameData\%MODNAME%\%MODNAME%.version
+set RELEASESDIR=releases
+set ZIP="c:\Program Files\7-zip\7z.exe"
+
+REM The following requires the JQ program, available here: https://stedolan.github.io/jq/download/
+set JD=C:\ProgramData\chocolatey\lib\jq\tools\jq.exe
+
+%JD%  ".VERSION.MAJOR" %VERSIONFILE% >tmpfile
+set /P major=<tmpfile
+
+%JD%  ".VERSION.MINOR" %VERSIONFILE% >tmpfile
+set /P minor=<tmpfile
+
+%JD%  ".VERSION.PATCH" %VERSIONFILE% >tmpfile
+set /P patch=<tmpfile
+
+%JD%  ".VERSION.BUILD" %VERSIONFILE% >tmpfile
+set /P build=<tmpfile
+del tmpfile
+set VERSION=%major%.%minor%.%patch%
+if "%build%" NEQ "0"  set VERSION=%VERSION%.%build%
+
+echo Version:  %VERSION%
+
+set FILE="%RELEASESDIR%\%MODNAME%-v%VERSION%.zip"
+IF EXIST %FILE% del /F %FILE%
+%ZIP% a -tzip %FILE% GameData
