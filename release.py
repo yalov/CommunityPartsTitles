709cc411c0c524a6ff578d3859d61c8221c39f8b
Alexander Yalov <alexander.yalov@gmail.com>
2019-02-04 16:01:35 +0300

    +glass, +pico, +scansat, ~kis, ~airplaneplus

---
+++ release.py
@@ -0,0 +1,209 @@
+"""requirements: Python3, pip install PyGithub
+Script loads release-arhive to github and spacedock
+you need to set values in the release.json
+also you need to place github-token and spacedock login+pass
+into a new file: release_token.json (alongside release.json)
+{
+    "GITHUB_TOKEN": "your_github_token"
+    "SPACEDOCK_LOGIN": "your_spacedock_login"
+    "SPACEDOCK_PASS": "your_spacedock_password"
+}
+"""
+
+import sys
+import json
+import os.path
+import re
+from shutil import copy
+import zipfile
+
+from github import Github
+from github import GithubException
+
+from release_spacedock_utils import GetSpacedockKSPVersions
+from release_spacedock_utils import GetSpacedockModDetails
+from release_spacedock_utils import PublishToSpacedock
+
+
+def zipdir(path, ziph):
+    """ recursive archiving path folder to ziph (zipfile handle) """
+    for root, _, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
+
+def archive_to(file):
+    """Archive GameData and Extras to .zip"""
+    if os.path.exists(file):
+        os.remove(file)
+
+    zipf = zipfile.ZipFile(file, 'x', zipfile.ZIP_DEFLATED)
+    if os.path.exists("GameData"):
+        zipdir("GameData", zipf)
+    if os.path.exists("Extras"):
+        zipdir("Extras", zipf)
+    zipf.close()
+    print("{}, files: {}, size: {} byte"
+          .format("valid ZIP" if zipfile.is_zipfile(file) else "FAIL",
+                  len(zipf.infolist()), os.path.getsize(file)))
+
+
+def get_version(version_file, obj="VERSION"):
+    """ get version from the version_file """
+    data = json.load(open(version_file))
+    ver = data[obj]
+    version = "{}.{}.{}".format(ver["MAJOR"], ver["MINOR"], ver["PATCH"])
+    if "BUILD" in ver and ver["BUILD"] != 0:
+        version += '.' + str(ver["BUILD"])
+    return version
+
+
+def get_description(path):
+    """ Get description of the last version in the changelog """
+    version = r"(#+ )?(Version )?\d\.\d\.\d(\.\d)?(/\d\.\d\.\d(\.\d)?)?"
+    changelog = open(path).read()
+    pattern = r"\n\s*\n{0}\n(?P<last>.+?)\n({0}|\n\Z|\Z)".format(version)
+    desc = re.search(pattern, changelog, re.DOTALL).group('last')
+    return desc
+
+
+def publish_to_github(token, mod_name, version, last_change, is_draft, is_prerelease, zip_file):
+    """create tag and publish a release"""
+    sys.stdout.write(" * Github connection...")
+    github = None
+    repo = None
+    user = None
+
+    try:
+        github = Github(token)
+        user = github.get_user()
+    except Exception:
+        print(" failed.")
+        sys.exit(-1)
+
+    try:
+        repo = user.get_repo(mod_name)
+        print(" user: {}, repo: {}".format(user.login, repo.name))
+    except Exception:
+        print(" failed to get {}".format(mod_name))
+        sys.exit(-1)
+
+    tags = repo.get_tags()
+    count = tags.totalCount
+
+    if count == 0 or (count > 0 and tags[0].name != version):
+        print(" * getting last commit sha...")
+        sha = repo.get_commits()[0].sha
+
+        try:
+            repo.create_git_ref('refs/tags/{}'.format(version), sha)
+        except GithubException:
+            print("   * could not create tag on the repo.")
+    else:
+        print(" * the tag "+version+" is found, skiping...")
+
+    rel = repo.create_git_release(tag=version, name="Version " + version,
+                                  message=last_change,
+                                  draft=is_draft, prerelease=is_prerelease)
+
+    print(" * uploading asset...")
+    rel.upload_asset(path=zip_file, content_type="application/zip")
+    print(" * success.")
+    print()
+
+
+if __name__ == '__main__':
+
+    jsn = json.load(open("release.json"))
+    tkn = json.load(open("release_token.json"))
+
+    MODNAME = jsn["MODNAME"]
+    MODFOLDER = jsn["MODFOLDER"]
+    VERSIONFILE = jsn["VERSIONFILE"]
+    RELEASESDIR = jsn["RELEASESDIR"]
+    CHANGELOG = jsn["CHANGELOG"]
+    DRAFT = jsn["DRAFT"]
+    PRERELEASE = jsn["PRERELEASE"]
+    SD_ID = jsn["SPACEDOCK_ID"]
+
+    TOKEN = tkn["GITHUB_TOKEN"]
+    SD_LOGIN = tkn["SPACEDOCK_LOGIN"]
+    SD_PASS = tkn["SPACEDOCK_PASS"]
+
+    if MODNAME == "auto":
+        parent = os.path.basename([x for x in sys.path if x][0])
+        onlyversionfiles = [f for f in os.listdir() if os.path.splitext(f)[1] == ".version"]
+        if (len(onlyversionfiles) == 1 and parent == os.path.splitext(onlyversionfiles[0])[0]):
+            MODNAME = parent
+        else:
+            print("You need to set up MODNAME in the release.json manually. Aborted.")
+            sys.exit(-1)
+
+    if MODFOLDER == "auto":
+        MODFOLDER = MODNAME
+
+    if VERSIONFILE == "auto":
+        VERSIONFILE = MODNAME + ".version"
+
+    copy(VERSIONFILE, "GameData/" + MODFOLDER)
+    copy(CHANGELOG, "GameData/" + MODFOLDER)
+
+    VERSION = get_version(VERSIONFILE)
+    KSP_VER = get_version(VERSIONFILE, "KSP_VERSION")
+    KSP_MIN = get_version(VERSIONFILE, "KSP_VERSION_MIN")
+    KSP_MAX = get_version(VERSIONFILE, "KSP_VERSION_MAX")
+
+    LAST_CHANGE = get_description(CHANGELOG)
+    ZIPFILE = os.path.join(RELEASESDIR, MODNAME + "-v" + VERSION + ".zip")
+
+    print("version: {}\nksp_ver: {}\nksp_min: {}\nksp_max: {}\n"
+          .format(VERSION, KSP_VER, KSP_MIN, KSP_MAX))
+    print("= start of desc ============")
+    print(LAST_CHANGE)
+    print("= end of desc ==============")
+    print("")
+
+    if os.path.exists(ZIPFILE):
+        print(ZIPFILE + " already exists.")
+        if input("Re-zip? [y/N]: ") == 'y':
+            archive_to(ZIPFILE)
+    else:
+        print("Creating "+ ZIPFILE +" ...")
+        archive_to(ZIPFILE)
+    
+    print("")
+    print("Create the tag, and publish a release with the asset?")
+    if input("[y/N]: ") == 'y':
+        publish_to_github(TOKEN, MODNAME, VERSION, LAST_CHANGE, DRAFT, PRERELEASE, ZIPFILE)
+
+    # ======================================
+
+    print("Accessing to Spacedock...")
+    all_versions = [v['name'] for v in GetSpacedockKSPVersions()]
+
+    if not all_versions:
+        print("Could not access to Spacedock.")
+        sys.exit(-1)
+
+    if KSP_VER not in all_versions:
+        print("KSP {} is not supported by Spacedock,\nlast supported version is KSP {}"
+              .format(KSP_VER, all_versions[0]))
+        print("Aborted.")
+        sys.exit(-1)
+
+    print("KSP {} is supported by Spacedock.".format(KSP_VER))
+
+    mod_details = GetSpacedockModDetails(SD_ID)
+
+    print("Spacedock info:\nID: {}, NAME: {}\nLast Release {} (KSP {})".format(
+        SD_ID, mod_details['name'],
+        mod_details['versions'][0]['friendly_version'],
+        mod_details['versions'][0]['game_version']
+        ))
+
+    print("Publish {} (KSP {}) to the Spacedock?".format(VERSION, KSP_VER))
+    if input("[y/N]: ") == 'y':
+        PublishToSpacedock(SD_ID, ZIPFILE, LAST_CHANGE, VERSION, KSP_VER, SD_LOGIN, SD_PASS)
+
+    input("Press Enter to close")
+    sys.exit(0)
---
+++ release_spacedock_utils.py
@@ -0,0 +1,229 @@
+"""utils for accessing to the spacedock"""
+# Public domain license.
+# Based on: https://github.com/ihsoft/KSPDev_ReleaseBuilder
+# $version: 2
+# -*- coding: utf-8 -*-
+
+import json
+import urllib.request
+import urllib.error
+import urllib.parse
+
+import io
+import random
+import string
+import ntpath
+
+
+# Endpoint for all the API requests
+API_BASE_URL = 'https://spacedock.info'
+
+# The actions paths.
+API_AUTHORIZE = '/api/login'
+API_UPDATE_MOD_TMPL = '/api/mod/{mod_id}/update'
+API_GET_VERSIONS = '/api/kspversions'
+API_GET_MOD = '/api/mod/{mod_id}'
+
+# The authorization cookie. It's only created once. To refresh it, simply
+#  set it to None.
+authorized_cookie = None
+
+
+def PublishToSpacedock(mod_id, filepath, changelog, mod_version, game_version, login, password):
+    """
+    Args:
+      mod_id: The mod ID to update.
+      filepath: A full or relative path to the local file.
+      changelog: The change log content.
+      mod_version: The version of the mod being published.
+      game_version: The KSP version to publish for.
+    Returns:
+      The response object.
+    """
+
+    headers, data = EncodeFormData([
+        {'name': 'version', 'data': mod_version},
+        {'name': 'changelog', 'data': changelog},
+        {'name': 'game-version', 'data': game_version},
+        {'name': 'notify-followers', 'data': 'yes'},
+        {'name': 'zipball', 'filename': filepath},
+    ])
+    url, headers = _GetAuthorizedEndpoint(
+        API_UPDATE_MOD_TMPL, headers, login, password, mod_id=mod_id)
+
+    resp = _CallAPI(url, data=data, headers=headers)
+
+    print('success.')
+    print('The new version is now available and the followers are notified!')
+
+    return resp
+
+
+def GetSpacedockModDetails(mod_id):
+    """Gets the mod informnation.
+    This call does NOT require authorization.
+    Args:
+      mod_id: The mod to request.
+    Returns:
+      The response object.
+    """
+    url = _MakeAPIUrl(API_GET_MOD, mod_id=mod_id)
+    response_obj, _ = _CallAPI(url, None, None)
+    return response_obj
+
+
+def GetSpacedockKSPVersions():
+    """Gets the available versions of the game.
+    This call does NOT require authorization.
+    Returns:
+      A list of objects: { 'name': <KSP name>, 'id': <Spacedock ID> }
+    """
+    response = _CallAPI(_MakeAPIUrl(API_GET_VERSIONS), None, None)
+    versions = [{'name': x['friendly_version'], 'id': x['id']} for x in response[0]]
+
+    return versions
+
+
+def _MakeAPIUrl(action_path, **kwargs):
+    """Makes a URL for the action."""
+    return API_BASE_URL + action_path.format(**kwargs)
+
+
+def _CallAPI(url, data, headers, raise_on_error=True):
+    """Invokes the API call."""
+    resp_obj = {'error': True, 'reason': 'unknown'}
+    try:
+        request = urllib.request.Request(url, data, headers=headers or {})
+        response = urllib.request.urlopen(request)
+        resp_obj = json.loads(response.read())
+        headers = response.info()
+    except urllib.error.HTTPError as ex:
+        resp_obj = {'error': True, 'reason': '%d - %s' % (ex.code, ex.reason)}
+        try:
+            resp_obj = json.loads(ex.read())
+        except Exception:
+            pass  # Not a JSON response
+        if ex.code == 401:
+            print("AuthorizationRequiredError")
+
+    if type(resp_obj) is dict and resp_obj.get('error'):
+        if raise_on_error:
+            print("BadResponseError")
+        return resp_obj, None
+    return resp_obj, headers
+
+
+def _GetAuthorizedEndpoint(api_path, headers, login, password, **kwargs):
+    """Gets API URL and the authorization headers.
+
+    The login/password must be set in the global variables API_LOGIN/API_PASS.
+    """
+    global authorized_cookie
+
+    url = _MakeAPIUrl(api_path, **kwargs)
+    if not headers:
+        headers = {}
+
+    if not authorized_cookie:
+        if not login or not password:
+            print("BadCredentialsError: API_LOGIN and/or API_PASS not set")
+            exit(0)
+
+        auth_headers, data = EncodeFormData([
+            {'name': 'username', 'data': login},
+            {'name': 'password', 'data': password},
+        ])
+        resp, auth_headers = _CallAPI(
+            API_BASE_URL + API_AUTHORIZE, data, auth_headers,
+            raise_on_error=False)
+        if resp['error']:
+            print("BadCredentialsError")
+            exit(0)
+        authorized_cookie = auth_headers['set-cookie']
+
+    headers['Cookie'] = authorized_cookie
+    return url, headers
+
+
+# Utils: Provides helpers to deal with the multipart/form-data MIME types.
+
+
+def EncodeFormData(fields):
+    """Encodes the provided arguments as the web form fields.
+
+    The argument must be an array of objects that define the fields to be passed:
+    - 'name': A required property than defines the name of the web form field.
+    - 'data': The raw data to pass. If it's of type 'string', then it's passed as
+      'text/plain'. Otherwise, it's serialized as JSON and passed as
+      'application/json'.
+    - 'filename': An optional property that designates a path to the binary file
+      to tansfer. The 'data' field is ignored in this case, and the real data is
+      read from the file. The data is passed as 'application/octet-stream', and
+      server will receive the file name part of the path. The part can be
+      relative, in which case it's counted realtive to the main module, or it can
+      be absolute.
+
+    Example:
+      [
+          { 'name': 'metadata', 'data': metadatObj },
+          { 'name': 'file', 'filename': '/home/me/releases/MyMod_v1.0.zip' }
+      ]
+
+    @param fields: An array of the fields to encode.
+    """
+    boundry = '----WebKitFormBoundary' + IdGenerator(16)
+
+    data_stream = io.BytesIO()
+    for field in fields:
+        filename = field.get('filename')
+        if filename:
+            with open(filename, 'rb') as f:
+                data = f.read()
+            content_type = 'application/octet-stream'
+            filename = ntpath.basename(filename)
+        else:
+            data = field['data']
+            if type(data) != str:
+                content_type = 'application/json'
+                data = json.dumps(data)
+            else:
+                content_type = 'text/plain; charset=utf-8'
+                data = data.encode('utf-8')
+        _WriteFormData(data_stream, boundry,
+                       field['name'], content_type, data, filename)
+
+    data_stream.write(b'--')
+    data_stream.write(boundry.encode())
+    data_stream.write(b'--')
+    data_stream.write(b'\r\n')
+
+    headers = {
+        'Content-Type': 'multipart/form-data; boundary=%s' % boundry,
+        'Content-Length': str(data_stream.tell()),
+    }
+    return headers, data_stream.getvalue()
+
+
+def IdGenerator(size, chars=string.ascii_letters + string.digits):
+    """Makes a unique random string of the requested size."""
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def _WriteFormData(stream, boundry, name, content_type, data, filename=None):
+    """Helper method to write a single web form field."""
+    stream.write(b'--')
+    stream.write(boundry.encode())
+    stream.write(b'\r\n')
+
+    if filename:
+        stream.write(('Content-Disposition: form-data; name="%s"; filename="%s"' %
+                      (name, filename)).encode())
+    else:
+        stream.write(('Content-Disposition: form-data; name="%s"' %
+                      (name)).encode())
+    stream.write(b'\r\n')
+    stream.write(('Content-Type: %s' % content_type).encode())
+    stream.write(b'\r\n')
+    stream.write(b'\r\n')
+    stream.write(data)
+    stream.write(b'\r\n')
